42459dc77bd566960f126aeb6a155a6a897ababb
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2020-06-28 15:49:35 +0530

    about us page done

--- lightbox.js
+++ lightbox.js
@@ -2,7 +2,7 @@ var lightbox = document.createElement('div');
 lightbox.id='lightbox';
 document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll('.grid-container img');
+const images = document.querySelectorAll(' img');
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
